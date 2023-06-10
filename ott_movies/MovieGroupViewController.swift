47866be14b37ed97aefaace408c699432493924a
mwjng <87010483+mwjng@users.noreply.github.com>
2023-06-11 00:56:30 +0900

    config collectionView

--- ott_movies/MovieGroupViewController.swift
+++ ott_movies/MovieGroupViewController.swift
@@ -9,33 +9,108 @@ import UIKit
 
 class MovieGroupViewController: UIViewController {
     
+    
     @IBOutlet weak var movieGroupTableView: UITableView!
-    var movies: [Movie] = []
+    var movieGroups: [[Movie]] = []
     var dbMemory: DbMemory!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        movieGroupTableView.dataSource = self
         dbMemory = DbMemory(parentNotification: nil)
-        queryGenre(genre: "Action")
+        queryGenres()
+        
+        movieGroupTableView.dataSource = self
+        movieGroupTableView.delegate = self
+        
     }
     
-    func queryGenre(genre: String) {
-        movies = dbMemory.queryMoviesByGenre(genre: genre)
+    
+    func queryGenres() {
+        let genres: [String] = ["Action", "Drama", "Comedy", "Thriller", "Adventure"]
+        
+        for genre in genres {
+            let movies = dbMemory.queryMoviesByGenre(genre: genre)
+            movieGroups.append(movies)
+        }
+        
         movieGroupTableView.reloadData()
     }
 }
 
-extension MovieGroupViewController: UITableViewDataSource {
-
+extension MovieGroupViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return movieGroups.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieGroupViewCell", for: indexPath)
-        let movie = movies[indexPath.row]
-        cell.textLabel!.text = "\(movie.title)"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "GenreTableViewCell", for: indexPath) as? GenreTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let movies = movieGroups[indexPath.section]
+        cell.setCollectionViewDataSourceDelegate(dataSourceDelegate: self, forRow: indexPath.section)
+        
         return cell
     }
 }
+
+extension MovieGroupViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let movies = movieGroups[collectionView.tag]
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGroupViewCell", for: indexPath) as! MovieGroupViewCell
+        let movies = movieGroups[collectionView.tag]
+        let movie = movies[indexPath.item]
+        cell.movieGenreView?.text = movie.title
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width * 0.7
+        let height = collectionView.bounds.height
+        return CGSize(width: width, height: height)
+    }
+}
+
+class GenreTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var genreCollectionView: UICollectionView!
+    var row: Int = 0
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureCollectionView()
+    }
+    
+    private func configureCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 10
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        genreCollectionView.collectionViewLayout = layout
+        
+        genreCollectionView.showsHorizontalScrollIndicator = false
+        
+    }
+    
+    func setCollectionViewDataSourceDelegate(dataSourceDelegate: UICollectionViewDataSource & UICollectionViewDelegateFlowLayout, forRow row: Int) {
+        self.row = row
+        genreCollectionView.dataSource = dataSourceDelegate
+        genreCollectionView.delegate = dataSourceDelegate
+        genreCollectionView.tag = row
+        genreCollectionView.reloadData()
+    }
+}
+
+class MovieGroupViewCell: UICollectionViewCell {
+    @IBOutlet weak var movieGenreView: UILabel!
+}
+
---
+++ ott_movies/SearchViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SearchViewController.swift
+//  ott_movies
+//
+//  Created by mwjng on 2023/06/10.
+//
+
+import Foundation
+
+class SearchViewController {
+    
+}
